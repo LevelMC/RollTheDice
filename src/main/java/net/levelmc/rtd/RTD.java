5ae6f25ee007abe262a71e1315bb0ebf8615556e
BuildTools <unconfigured@null.spigotmc.org>
2023-07-23 18:36:57 +0200

    Initial commit

---
+++ src/main/java/net/levelmc/rtd/RTD.java
@@ -0,0 +1,17 @@
+package net.levelmc.rtd;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RTD extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
